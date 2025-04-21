90b2270c20af05775a5851d4d960ef3bfdde5933
Dayvis <amdayvis39@gmail.com>
2025-04-21 15:52:46 -0500

    21/04/2025 2

---
+++ PracticasLogica/src/If/SiEsMayor.java
@@ -0,0 +1,14 @@
+package If;
+
+public class SiEsMayor {
+    public static void main(String[] args) {
+        int nume = 0;
+                
+        if (nume == 1) {
+            System.out.println("El numero es uno");
+        }
+        else {
+            System.out.println("El numero no es uno");
+        }
+    }
+}
