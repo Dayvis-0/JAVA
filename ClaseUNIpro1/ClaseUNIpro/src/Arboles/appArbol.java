fdebeed1c5f07e9b053f3d92ac5283313d0ae6c6
Dayvis <amdayvis39@gmail.com>
2025-05-21 22:31:40 -0500

    22/05/2025 1

---
+++ ClaseUNIpro1/ClaseUNIpro/src/Arboles/appArbol.java
@@ -0,0 +1,232 @@
+package Arboles;
+
+public class appArbol extends javax.swing.JFrame {
+    private String padre, hijo;
+    private cArbol pArbol;
+            
+    public appArbol() {
+        initComponents();
+        
+        hijo = "";
+        padre = "";
+        pArbol = new cArbol();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ta_rta = new javax.swing.JTextArea();
+        tf_padre = new javax.swing.JTextField();
+        tf_hijo = new javax.swing.JTextField();
+        bu_agregar = new javax.swing.JButton();
+        bu_modificar = new javax.swing.JButton();
+        bu_aliminar = new javax.swing.JButton();
+        cb_operaciones = new javax.swing.JComboBox<>();
+        bu_ejecutar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Ingrese Padre:");
+
+        jLabel2.setText("Ingrese Hijo:");
+
+        ta_rta.setColumns(20);
+        ta_rta.setRows(5);
+        jScrollPane1.setViewportView(ta_rta);
+
+        bu_agregar.setText("Agregar");
+        bu_agregar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bu_agregarMouseClicked(evt);
+            }
+        });
+
+        bu_modificar.setText("Modificar");
+        bu_modificar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bu_modificarMouseClicked(evt);
+            }
+        });
+
+        bu_aliminar.setText("Eliminar");
+        bu_aliminar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bu_aliminarMouseClicked(evt);
+            }
+        });
+
+        cb_operaciones.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Altura", "Nivel", "Contar nodos", "Es hijo?", "Es padre?", "Pre-orden", "In-orden", "Pos-orden", "Contar padres", "Contar hijos", "Existe?", "Recorrer hijos", "Recorrer padres", "Mayor?", "Recorrer: " }));
+
+        bu_ejecutar.setText("Ejecutar");
+        bu_ejecutar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bu_ejecutarMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(bu_agregar, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(35, 35, 35)
+                        .addComponent(bu_modificar, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(bu_aliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(cb_operaciones, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
+                        .addComponent(bu_ejecutar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(tf_padre, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(tf_hijo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(67, 67, 67)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(31, 31, 31))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(tf_padre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(tf_hijo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(bu_agregar)
+                        .addComponent(bu_modificar)
+                        .addComponent(bu_aliminar)
+                        .addComponent(cb_operaciones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(bu_ejecutar))
+                .addContainerGap(34, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void bu_agregarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bu_agregarMouseClicked
+        // TODO add your handling code here:
+        padre = tf_padre.getText();
+        hijo = tf_hijo.getText();
+        // 
+        pArbol.agregar(padre, hijo);
+        
+        ta_rta.setText(pArbol.imprimiNodos());
+    }//GEN-LAST:event_bu_agregarMouseClicked
+
+    private void bu_modificarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bu_modificarMouseClicked
+        // TODO add your handling code here:
+        
+        String raizAmodificar = tf_padre.getText(); 
+        String nuevoValor = tf_hijo.getText(); 
+        
+        pArbol.modificar(raizAmodificar, nuevoValor);
+        
+        ta_rta.setText(pArbol.imprimiNodos());
+    }//GEN-LAST:event_bu_modificarMouseClicked
+
+    private void bu_aliminarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bu_aliminarMouseClicked
+        // TODO add your handling code here:
+        String raizEliminar = tf_padre.getText();
+        
+        pArbol.eliminar(raizEliminar);
+        
+        ta_rta.setText(pArbol.imprimiNodos());
+    }//GEN-LAST:event_bu_aliminarMouseClicked
+
+    private void bu_ejecutarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bu_ejecutarMouseClicked
+        // TODO add your handling code here:    
+        int opci = cb_operaciones.getSelectedIndex();
+        
+        switch(opci) {
+            case 0: ta_rta.setText("La altura es: " + pArbol.altura()); break;
+            case 1: ta_rta.setText("La nivel es: " + pArbol.nivel(tf_padre.getText(), 0)); break;
+            case 2: ta_rta.setText("El numero de nodos es: " + pArbol.contarNodos()); break;
+            case 3: ta_rta.setText("Es hijo? " + pArbol.esHijo(tf_padre.getText())); break;
+            case 4: ta_rta.setText("Es padre? " + pArbol.esPadre(tf_padre.getText())); break;
+            case 5: ta_rta.setText("Pre-orden " + pArbol.preOrden()); break;
+            case 6: ta_rta.setText("In-orden" + pArbol.inOrden()); break;
+            case 7: ta_rta.setText("Pos-orden" + pArbol.posOrden()); break;
+            case 8: ta_rta.setText("Contar padres: " + pArbol.padres()); break;
+            case 9: ta_rta.setText("Contar hijos: " + pArbol.hijos()); break;
+            case 10: ta_rta.setText("Existe: " + pArbol.existe(tf_padre.getText())); break;
+            case 11: ta_rta.setText("Recorrer hijos" + pArbol.recorrerHijos()); break;
+            case 12: ta_rta.setText("Recorrer padres" + pArbol.recorrerPadres()); break;
+            case 13: ta_rta.setText("Mayor? " + pArbol.mayor()); break;
+            case 19: ta_rta.setText("Recorrer: "+pArbol.recorrerNodos()); break;
+        }
+    }//GEN-LAST:event_bu_ejecutarMouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(appArbol.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(appArbol.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(appArbol.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(appArbol.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                appArbol arb1 = new appArbol();
+                
+                arb1.setVisible(true);
+                arb1.setLocationRelativeTo(null);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bu_agregar;
+    private javax.swing.JButton bu_aliminar;
+    private javax.swing.JButton bu_ejecutar;
+    private javax.swing.JButton bu_modificar;
+    private javax.swing.JComboBox<String> cb_operaciones;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea ta_rta;
+    private javax.swing.JTextField tf_hijo;
+    private javax.swing.JTextField tf_padre;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ ClaseUNIpro1/ClaseUNIpro/src/Arboles/cArbol.java
@@ -0,0 +1,412 @@
+package Arboles;
+
+public class cArbol {
+    // Atributo 
+    private Object aRaiz;
+    private cArbol aHijo;
+    private cArbol aHermano;
+    // constructores
+    public cArbol() {
+        aRaiz = null;
+        aHijo = null;
+        aHermano = null;
+    }
+    public cArbol(Object pRaiz) {
+        aRaiz = pRaiz;
+        aHijo = null;
+        aHermano = null;
+    }
+    // Modificadores
+    public void mRaiz(Object pRaiz) { aRaiz = pRaiz; }
+    public void mHijo(cArbol pHijo) { aHijo = pHijo; }
+    public void mHermano(cArbol pHermano) { aHermano = pHermano; }
+    public boolean estaVacio() { return (aRaiz == null); }
+    // Selectore
+    public Object sRaiz() { return aRaiz; }
+    public cArbol sHijo() { return aHijo; }
+    public cArbol sHemano() { return aHermano; }
+    // Metodos, busca si existe
+    public cArbol buscarSubArbol(Object pRaiz) {
+        cArbol subArbol = null;
+        
+        if (aRaiz != null) {
+            if (aRaiz.equals(pRaiz)) {
+                subArbol = this;
+            }
+            if (subArbol == null && aHijo != null) {
+                subArbol = aHijo.buscarSubArbol(pRaiz);
+            }
+            if (subArbol == null && aHermano != null) {
+                subArbol = aHermano.buscarSubArbol(pRaiz);
+            }
+        }
+        return subArbol;
+    }
+    // Agregar hermano 
+    public boolean agregarHermano(Object pHermano) {
+        boolean respuesta = false;
+        
+        if (aHermano == null) {
+            aHermano = new cArbol(pHermano);
+            respuesta = true;
+        }
+        return respuesta;
+    }
+    // Agregar hijo
+    public boolean agregarHijo(Object pHijo) {
+        boolean respuesta = false;
+        
+        if (aHijo == null) {
+            aHijo = new cArbol(pHijo);
+            respuesta = true;
+        }
+        else {
+            respuesta = agregarHermano(pHijo);
+        }
+        return respuesta;
+    }
+    // Agregar
+    public boolean agregar(Object pPadre, Object pHijo) {
+        boolean respuesta = false;
+        cArbol subArbolEncotrado = null;
+        
+        if (aRaiz == null) {
+            aRaiz = pHijo;
+            respuesta = true;
+        }
+        else {
+            subArbolEncotrado = buscarSubArbol(pPadre);
+            
+            if (subArbolEncotrado != null) {
+                respuesta = subArbolEncotrado.agregarHijo(pHijo);
+            }
+        }
+        return respuesta;
+    }
+    // Retornar los datos del arbol en cadena
+   public String imprimiNodos() {
+       String cadenaDeNodos = "";
+       
+       if (aRaiz != null) {
+           cadenaDeNodos = cadenaDeNodos + aRaiz;
+       }
+       
+       if (aHijo != null) {
+           cadenaDeNodos = cadenaDeNodos + " " + aHijo.imprimiNodos();
+       }
+       
+       if (aHermano != null) {
+            cadenaDeNodos = cadenaDeNodos + " " + aHermano.imprimiNodos();
+       }
+       return cadenaDeNodos;
+   }
+   
+   public boolean modificar(Object pRaiz, Object pNvalor) {
+        boolean rta = false;
+        cArbol arbolEncontrado;
+        
+        arbolEncontrado = buscarSubArbol(pRaiz);
+        
+        if (arbolEncontrado != null) {
+            arbolEncontrado.mRaiz(pNvalor);
+        }
+        return rta;
+   }
+   
+    public boolean esHijo(Object pRaiz) {
+        boolean rta = false;
+        if(!estaVacio()) {
+            if(aRaiz.equals(pRaiz)) { 
+                if(aHijo == null) { rta = true; }
+            }
+            else{
+                if(aHijo != null) { rta = aHijo.esHijo(pRaiz); }
+                if(!rta && aHermano != null) { rta = aHermano.esHijo(pRaiz); }
+            }
+        }
+        return rta;
+    }
+    
+    public boolean esPadre(Object pRaiz) {
+        boolean rta = false;
+        
+        if(!estaVacio()) {
+            if(aRaiz.equals(pRaiz) && aHijo != null) { rta = true;}
+            if(!rta && aHijo != null) { rta = aHijo.esPadre(pRaiz); }
+            if(!rta && aHermano != null ) {rta = aHermano.esPadre(pRaiz); }
+        }
+        
+        return rta;
+    }
+    
+    public String hermanoPreO() {
+        String rta = "";
+        if(aHermano != null) {
+            rta = aHermano.preOrden();
+            rta = rta + aHermano.hermanoPreO();
+        }
+        return rta;
+    }
+    
+    public String preOrden() {
+        String rta = "";
+        if(!estaVacio()) {
+            rta = aRaiz + " ";
+            if(aHijo != null) {
+                rta = rta + aHijo.preOrden();
+                rta = rta + aHijo.hermanoPreO();
+            }
+        }
+        return rta;
+    }
+    
+    public Object padre(Object pRaiz) {
+        Object rta = null;
+        
+        return rta;
+    }
+    
+    public String hermanoInO() {
+        String rta = "";
+        if(aHermano != null) {
+            rta = aHermano.inOrden();
+            rta = rta + aHermano.hermanoInO();
+        }
+        return rta;
+    }
+    
+    public String inOrden() {
+        String rta = "";
+        if(!estaVacio()) {
+            if (aHijo != null) { rta = aHijo.inOrden(); }
+            rta = rta + " " + aRaiz;
+            if(aHijo != null) { rta = rta + aHijo.hermanoInO(); }
+        }
+        return rta;
+    }
+    
+    public String hermanoPosO() {
+        String rta = "";
+        if(aHermano != null) {
+            rta = aHermano.posOrden();
+            rta = rta + aHermano.hermanoPosO();
+        }
+        return rta;
+    }
+    
+    public String posOrden() {
+        String rta = "";
+        if(!estaVacio()) {
+            if(aHijo != null) {
+                rta = aHijo.posOrden();
+                rta = rta + aHijo.hermanoPosO();
+            }
+            rta = rta + " " + aRaiz;
+        }
+        return rta;
+    }
+    
+    public int padres() {
+        int rta = 0;
+        if(!estaVacio()) {
+            if(aHijo != null) { rta = 1 + aHijo.padres(); }
+            if(aHermano != null) { rta = rta + aHermano.padres(); }
+        }
+        return rta;
+    }
+    
+    public int hijos() {
+        int rta = 0;
+        if(!estaVacio()) {
+            if(aHijo != null) { rta = 1; }
+            if(aHijo != null) { rta = aHijo.hijos(); }
+            if(aHermano != null) { rta = rta + aHermano.hijos(); }
+        }
+        return rta;
+    }
+    
+    public boolean existe(Object pRaiz) {
+        boolean rta = false;
+        if(!estaVacio()) {
+            if(aRaiz.equals(pRaiz)) { rta = true; }
+            else {
+                if(aHijo != null) { rta = aHijo.existe(pRaiz); } 
+                if(!rta && aHermano != null) { rta = aHermano.existe(pRaiz); }
+            }
+        }
+        return rta;
+    }
+    
+    public cArbol buscarPadre(Object pHijo) {
+        cArbol arbolPadre;
+        arbolPadre = null;
+        
+        if(aRaiz != null) {
+            if(aHijo != null) {
+                if(pHijo.equals(aHijo.sRaiz())) {
+                    arbolPadre = this;
+                }
+                else {
+                    arbolPadre = aHijo.buscarPadre(pHijo);
+                }
+            }
+            if(arbolPadre == null && aHermano != null) {
+                if(pHijo.equals(aHermano.sRaiz())) {
+                    arbolPadre = this;
+                }
+                else {
+                    arbolPadre = aHermano.buscarPadre(pHijo);
+                }
+            }
+        }
+        return arbolPadre;
+    }
+    
+    public boolean eliminar(Object pRaiz) {
+        boolean respuesta = false;
+        cArbol arbolEliminar;
+        cArbol padreEncontrado;
+        
+        arbolEliminar = buscarSubArbol(pRaiz);
+        if(arbolEliminar != null) {
+            if(aRaiz.equals(arbolEliminar.sRaiz())){
+                aRaiz = null;
+                aHijo = null;
+                aHermano = null;
+            }
+            else {
+                padreEncontrado = buscarPadre(pRaiz);
+                if(padreEncontrado != null) {
+                    if(pRaiz.equals(padreEncontrado.sHijo().sRaiz())) {
+                        padreEncontrado.mHijo(null);
+                        padreEncontrado.mHijo(padreEncontrado.sHemano());
+                        padreEncontrado.mHermano(null);
+                        respuesta = true;
+                    }
+                    else {
+                        padreEncontrado.mHermano(null);
+                        respuesta = true;
+                    }
+                }
+            }
+        }
+        return respuesta;
+    }
+   
+    public int contarNodos() {
+        int numeroDeNodos = 0;
+        
+        if (aRaiz != null) {
+            numeroDeNodos = 1;
+            if(aHijo != null) {
+                numeroDeNodos = numeroDeNodos +aHijo.contarNodos();
+            }
+            if(aHermano != null) {
+                numeroDeNodos = numeroDeNodos +aHermano.contarNodos();
+            }
+        }
+        return numeroDeNodos;
+    }
+    
+    public int altura() {
+        int altura = 0;
+        
+        if(!estaVacio()) {
+            int alturaHijo = 0;
+            int alturaHermano = 0;
+            
+            if(aHijo != null) {
+                alturaHijo = 1 + aHijo.altura();
+            }
+            if(aHermano != null) {
+                alturaHermano = 1 + aHermano.altura();
+            }
+            
+            altura = (alturaHijo > alturaHermano ? alturaHijo : alturaHermano);
+        }
+        return altura;
+    }
+    
+    public int nivel(Object pHijo, int pNivel) {
+        int nivel = 0;
+        
+        if (!estaVacio()) {
+            if(aRaiz.equals(pHijo)) {
+                nivel = pNivel;
+            }
+            if(nivel == 0 && aHijo != null) {
+                nivel = aHijo.nivel(pHijo, pNivel + 1);
+            }
+            if(nivel == 0 && aHermano != null) {
+                nivel = aHermano.nivel(pHijo, pNivel + 1);
+            }
+        }
+        
+        return nivel;
+    }
+    
+    public String recorrerPadres() {
+        String rta = "";
+        
+        if(!estaVacio()) {
+            if(aHijo != null) { rta = aRaiz +" " + aHijo.recorrerPadres(); }
+            if(aHermano != null) { rta = rta  + aHermano.recorrerPadres();}
+        }
+        return rta;
+    }
+    
+    public String recorrerHijos() {
+        String rta = "";
+        if(!estaVacio()) {
+            if(aHijo != null) { rta = aRaiz + " " + aHijo.recorrerHijos(); }
+            if(aHermano != null) { rta = rta  + aHermano.recorrerHijos(); } 
+        }
+        return rta;
+    }
+    
+    public String mayor() {
+        String rta = "", aux = "";
+        if(!estaVacio()) {
+            rta = aRaiz + "";
+            if(aHijo != null) { aux = aHijo.mayor(); }
+            if(aux.compareTo(rta) > 0) { rta = aux; }
+            if(aHijo != null) { aux = aHermano.mayor(); }
+            if(aux.compareTo(rta) > 0) { rta = aux; }
+        }
+        
+        return  rta;
+    }
+    
+    public String recorrerNodos() {
+        String rta = "";
+        
+        if(!estaVacio()) {
+            rta = aRaiz + " ";
+            if(aHijo != null) { rta = rta + recorrerNodos(); }
+            if(aHermano != null) { rta = rta + recorrerNodos(); }
+        }
+        
+        return rta;
+    }
+    
+    public static void main(String[] args) {
+        cArbol arb1 = new cArbol();
+        
+        arb1.agregar(null, "a");
+        arb1.agregar("a", "100");
+        arb1.agregar("a", "x");
+        arb1.agregar("a", "y");
+        arb1.agregar("a", "b");
+        
+        System.out.println(arb1.imprimiNodos());
+        System.out.println("El numero de nodos es:  " + arb1.contarNodos());
+        if (arb1.eliminar("20")) {
+            System.out.println("Eliminado ");
+            System.out.println(arb1.imprimiNodos());
+        }
+        
+        System.out.println("La altura del arbol es: " + arb1.altura());
+        System.out.println("El nivel del nodo 20: " + arb1.nivel("2", 0));
+        System.out.println("El mayor es: " + arb1.mayor());
+    }
+}
