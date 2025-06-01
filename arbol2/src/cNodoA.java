dbcdae9061a937e4be3b40314d0be6e9ed52b01a
Dayvis <amdayvis39@gmail.com>
2025-06-01 17:39:33 -0500

    01/06/2025 1

--- arbol2/src/cNodoA.java
+++ arbol2/src/cNodoA.java
@@ -3,17 +3,13 @@ public class cNodoA {
     private Object aElemento;
     private cNodoA aPhijo, aShermano;
     //... constructor
-    protected cNodoA(){ aElemento= null;
-        aPhijo= null; aShermano= null;
-    }
-    protected cNodoA(Object pElemento){ aElemento= pElemento;
-        aPhijo= null; aShermano= null;
-    }
+    protected cNodoA(){ aElemento= null; aPhijo= null; aShermano= null; }
+    protected cNodoA(Object pElemento){ aElemento= pElemento; aPhijo= null; aShermano= null; }
     //... modificadores
     public void mElemento(Object pElemento){ aElemento= pElemento; }
     public void mPhijo(cNodoA pPhijo){ aPhijo= pPhijo; }
     public void mShermano(cNodoA pShermano){ aShermano= pShermano; }
-    //... modificadores
+    //... selectores
     public Object sElemento(){ return aElemento; }
     public cNodoA sPhijo(){ return aPhijo; }
     public cNodoA sShermano(){ return aShermano; }
