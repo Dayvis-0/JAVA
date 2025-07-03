f0b5f303df60bc285f4f728f74da149bb554bc86
dayvis <amdayvis39@gmail.com>
2025-07-03 17:55:02 -0500

    03/07/2025

--- ArbolBDayvis/src/appArbolBI.java
+++ ArbolBDayvis/src/appArbolBI.java
@@ -1,24 +1,49 @@
 public class appArbolBI extends javax.swing.JFrame {
-    
+    private cArbolBI arbbi;
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(appArbolBI.class.getName());
     
     public appArbolBI() {
         initComponents();
+        arbbi = new cArbolBI();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        la_nodo = new javax.swing.JLabel();
+        tf_nodo = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ta_rta = new javax.swing.JTextArea();
+        cb_operaciones = new javax.swing.JComboBox<>();
+        bu_eliminar = new javax.swing.JButton();
+        bu_ejecutor = new javax.swing.JButton();
+        bu_agregar = new javax.swing.JButton();
+        bu_limpiar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Nodo:");
+        la_nodo.setText("Nodo:");
 
-        jTextField1.setText("jTextField1");
+        ta_rta.setColumns(20);
+        ta_rta.setRows(5);
+        jScrollPane1.setViewportView(ta_rta);
+
+        cb_operaciones.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Agregar" }));
+
+        bu_eliminar.setText("Eliminar");
+
+        bu_ejecutor.setText("Ejecutar");
+
+        bu_agregar.setText("Agregar");
+        bu_agregar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bu_agregarMouseClicked(evt);
+            }
+        });
+
+        bu_limpiar.setText("Limpiar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -26,23 +51,59 @@ public class appArbolBI extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(26, 26, 26)
-                .addComponent(jLabel1)
-                .addGap(50, 50, 50)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(269, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(la_nodo)
+                        .addGap(18, 18, 18)
+                        .addComponent(tf_nodo, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(bu_agregar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(bu_eliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(cb_operaciones, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(bu_limpiar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(bu_ejecutor, javax.swing.GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(17, 17, 17)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(209, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(la_nodo)
+                            .addComponent(tf_nodo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(bu_eliminar)
+                            .addComponent(bu_agregar)
+                            .addComponent(bu_limpiar))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(cb_operaciones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(bu_ejecutor)))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bu_agregarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bu_agregarMouseClicked
+        // TODO add your handling code here:
+        if(arbbi.agregar(tf_nodo.getText())){
+            ta_rta.setText("Elemento insertado");
+        } else {
+            ta_rta.setText("Elemento no insertado");
+        }
+    }//GEN-LAST:event_bu_agregarMouseClicked
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -67,7 +128,14 @@ public class appArbolBI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton bu_agregar;
+    private javax.swing.JButton bu_ejecutor;
+    private javax.swing.JButton bu_eliminar;
+    private javax.swing.JButton bu_limpiar;
+    private javax.swing.JComboBox<String> cb_operaciones;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel la_nodo;
+    private javax.swing.JTextArea ta_rta;
+    private javax.swing.JTextField tf_nodo;
     // End of variables declaration//GEN-END:variables
 }
--- ArbolBDayvis/src/cArbolBI.java
+++ ArbolBDayvis/src/cArbolBI.java
@@ -10,7 +10,25 @@ public class cArbolBI {
     // selector
     public cNodoAB sRaiz() { return aRaiz; }
     // otros metodos
-    public boolean estaVacio() { return (aRaiz == null);}
+    public boolean estaVacio() { return (aRaiz == null); }
+    public cNodoAB arbol(Object pElemento) {
+        cNodoAB rta = null;
+        if(!estaVacio()) {
+            boolean encontrado = false;
+            rta = aRaiz;
+            while(!encontrado && rta != null) {
+                if(pElemento.equals(rta.sElemento())) { encontrado = true;  }
+                else {
+                    if(pElemento.toString().compareTo(rta.sElemento().toString()) < 0) {
+                        rta = rta.sSubArbolIzq();
+                    } else {
+                        rta = rta.sSubArbolDer();
+                    }
+                }
+            }
+        }
+        return rta;
+    }
     public cNodoAB buscar(Object pElemento) {
         cNodoAB rta = null;
         if(!estaVacio()) {
@@ -28,7 +46,7 @@ public class cArbolBI {
     }
     public boolean agregar(Object pElemento) { 
         boolean rta = false;
-        if(!estaVacio()) { aRaiz = new cNodoAB(pElemento); rta = true; }
+        if(estaVacio()) { aRaiz = new cNodoAB(pElemento); rta = true; }
         else {
             cNodoAB dir = buscar(pElemento);
             if(pElemento.toString().compareTo(dir.sElemento().toString()) <= 0) {
@@ -54,4 +72,81 @@ public class cArbolBI {
             }
         }return rta;
     }
+    public cNodoAB maximo() {
+        cNodoAB rta = null;
+        if(!estaVacio()){
+            rta = aRaiz;
+            while(rta.sSubArbolDer() != null) { rta = rta.sSubArbolDer(); }
+        }return rta;
+    }
+    public cNodoAB minimo() {
+        cNodoAB rta = null;
+        if(!estaVacio()){
+            rta = aRaiz;
+            while(rta.sSubArbolIzq()!= null) { rta = rta.sSubArbolIzq(); }
+        }return rta;
+    }
+    public cNodoAB maximo(cNodoAB pMaximo) {
+        cNodoAB rta = pMaximo;
+        while(rta.sSubArbolDer() != null) { rta = rta.sSubArbolDer(); }
+        return rta;
+    }
+    public void limpiar() {
+        if(!estaVacio()) {
+            Stack<cNodoAB> pila = new Stack<>();
+            cNodoAB dir = null;
+            pila.push(aRaiz);
+            while(!pila.isEmpty()) {
+                dir = pila.pop();
+                if(dir.sSubArbolIzq() != null){
+                    if(dir.sSubArbolIzq().sElemento() == null) {
+                        if(dir.sSubArbolIzq().sSubArbolIzq() == null && dir.sSubArbolIzq().sSubArbolDer() == null) {
+                            dir.mSubArbolIzq(null);
+                        } else if(dir.sSubArbolIzq().sSubArbolIzq() != null) {
+                            dir.sSubArbolIzq().mElemento(dir.sSubArbolIzq().sSubArbolIzq().sElemento());
+                            dir.sSubArbolIzq().mSubArbolDer(dir.sSubArbolIzq().sSubArbolIzq().sSubArbolDer());
+                            dir.sSubArbolIzq().mSubArbolIzq(dir.sSubArbolIzq().sSubArbolIzq().sSubArbolIzq());
+                        } else if(dir.sSubArbolIzq().sSubArbolDer() != null) {
+                            dir.sSubArbolIzq().mElemento(dir.sSubArbolIzq().sSubArbolDer().sElemento());
+                            dir.sSubArbolIzq().mSubArbolIzq(dir.sSubArbolIzq().sSubArbolDer().sSubArbolIzq());
+                            dir.sSubArbolIzq().mSubArbolDer(dir.sSubArbolIzq().sSubArbolDer().sSubArbolDer());
+                        }
+                    }
+                }
+                if(dir.sSubArbolIzq() != null) { pila.push(dir.sSubArbolIzq()); }
+                if(dir.sSubArbolDer()!= null) { pila.push(dir.sSubArbolDer()); }
+            }
+        }
+    }
+    public boolean eliminar(Object pElemento){
+        boolean rta = false;
+        if(!estaVacio()) {
+            cNodoAB max = null, dir = arbol(pElemento);
+            if(dir != null) {
+                rta = true;
+                if(dir == aRaiz && dir.sSubArbolIzq() == null && dir.sSubArbolDer() == null) {
+                    aRaiz = null;
+                }
+                else {
+                    if(dir.sSubArbolIzq() == null && dir.sSubArbolDer() == null) { dir.mElemento(null); }
+                    else if(dir.sSubArbolIzq() != null && dir.sSubArbolDer() != null) {
+                        max = maximo(dir.sSubArbolIzq());
+                        dir.mElemento(max.sElemento());
+                        max.mElemento(null);
+                    }
+                    else if(dir.sSubArbolIzq() != null) {
+                        dir.mElemento(dir.sSubArbolIzq().sSubArbolDer());
+                        dir.mSubArbolDer(dir.sSubArbolIzq().sSubArbolDer());
+                        dir.mSubArbolIzq(dir.sSubArbolIzq().sSubArbolIzq());
+                    } else {
+                        dir.mElemento(dir.sSubArbolDer().sElemento());
+                        dir.mSubArbolDer(dir.sSubArbolDer().sSubArbolIzq());
+                        dir.mSubArbolIzq(dir.sSubArbolDer().sSubArbolDer());
+                    }
+                }
+            }
+        }
+        if(rta) { limpiar(); }
+        return rta;
+     }
 }
